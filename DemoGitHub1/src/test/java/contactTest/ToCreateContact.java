0f1b75dedb985b169bc6ea7838a2edbebd853ddf
Biswa <Biswa@Biswa-HP>
2024-08-13 11:19:18 +0530

    By Biswa

--- DemoGitHub1/src/test/java/contactTest/ToCreateContact.java
+++ DemoGitHub1/src/test/java/contactTest/ToCreateContact.java
@@ -4,7 +4,8 @@ public class ToCreateContact {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Organization create successfully");
+		System.out.println("contact create successfully");
+		System.out.println("contact by bIswa");
 
 	}
 
--- DemoGitHub1/src/test/java/organizationTest/ToCreateOrganization.java
+++ DemoGitHub1/src/test/java/organizationTest/ToCreateOrganization.java
@@ -5,6 +5,7 @@ public class ToCreateOrganization {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Organization create successfully");
+		System.out.println("Org by bIswa");
 
 	}
 
