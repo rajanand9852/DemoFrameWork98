2d4d075cfcbbf23690e000ff00cb9332a4d7008a
rajanand9852 <romilraj9852@gmail.com>
2025-03-03 23:04:14 +0530

    Delete DemoGitHub1 directory

--- DemoGitHub1/src/test/java/CalenderTest/ToCreateCalender.java
+++
@@ -1,9 +0,0 @@
-package CalenderTest;
-
-public class ToCreateCalender {
-
-	public static void main(String[] args) {
-		System.out.println("calender created");
-	}
-
-}
--- DemoGitHub1/src/test/java/LeadTest/ToCreatelead.java
+++
@@ -1,9 +0,0 @@
-package LeadTest;
-
-public class ToCreatelead {
-
-	public static void main(String[] args) {
-		System.out.println("lead created");
-	}
-
-}
